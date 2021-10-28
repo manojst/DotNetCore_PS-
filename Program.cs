7a2aad82d9bba0104f8e91c98c3ab4fc1dbafae0
Manojkumar Gnanasekaran(UST <172389@ust-global.com>
2021-10-28 11:15:28 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B_P_DotNetCore_PS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DotNet Core Build and Publish in Powershell!");
+        }
+    }
+}
